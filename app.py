5a532abde8c94e8683bdee77419ceeecc6899b44
khalilabid95 <ounshadriche@gmail.com>
2026-02-20 14:38:40 +0300

    Refactor app initialization and user loader setup

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
-from flask import Flask, render_template, jsonify, request
-
-from dotenv import load_dotenv
-from rag_chatbot import rag_chat_medical
-from src.prompt import *
-
-import os
+from flask import Flask
+from config import Config
+from extensions import db, login_manager
+from models import User
+from routes import register_blueprints
 
 
-#Initialize flask app
-app = Flask(__name__)
-load_dotenv()
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Initialize extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # User loader
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    # Register blueprints
+    register_blueprints(app)
+
+    # Create DB tables
+    with app.app_context():
+        db.create_all()
+
+    return app
 
 
-
-@app.route("/")
-def index():
-    return render_template('chat.html')
-
-@app.route("/get_msg_chat_medical", methods =["GET","POST"])
-def chatbot():
-    msg = request.form['msg']
-    input = msg
-    return rag_chat_medical(input)
-    
 if __name__ == '__main__':
+    app = create_app()
     app.run(host="0.0.0.0", port=8080, debug=True)
